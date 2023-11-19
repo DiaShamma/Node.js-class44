eb993773791cd20c28945c7ff8ac67cc5b702c51
Your GitHub Username <Your GitHub Email>
2023-11-19 17:26:47 +0100

    Updated server.js and test file based on my mentor review

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -13,22 +13,39 @@ app.get('/', (req, res) => {
 
 const PORT = 3000;
 
+
+
 app.post('/weather', async (req, res) => {
   const cityName = req.body.cityName;
+
+  // Check if cityName is missing in the request
+  if (!cityName) {
+    return res.status(400).json({ error: 'CityName is required in the request body' });
+  }
 
   try {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`);
     const weatherData = await response.json();
 
+    if (response.status === 401) {
+      return res.status(401).json({ error: 'Unauthorized: API key is invalid or missing' });
+    }
+
+    if (response.status >= 500) {
+      return res.status(response.status).json({ error: 'Server error from OpenWeatherMap API' });
+    }
+
     if (weatherData.cod === '404') {
-      res.json({ weatherText: 'City is not found!' });
-    } else {
+      return res.json({ weatherText: 'City is not found!' });
+    } else if (response.status === 200) {
       const temperature = weatherData.main.temp;
-      res.json({ weatherText: `Temperature in ${cityName}: ${temperature}°C` });
+      return res.json({ weatherText: `Temperature in ${cityName}: ${temperature}°C` });
+    } else {
+      return res.status(500).json({ error: 'Unknown error from OpenWeatherMap API' });
     }
   } catch (error) {
     console.error('Error fetching weather data:', error);
-    res.status(500).json({ error: 'Error fetching weather data' });
+    return res.status(500).json({ error: 'Error fetching weather data' });
   }
 });
 
